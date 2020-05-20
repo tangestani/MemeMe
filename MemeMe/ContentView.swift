1c335c91a7949354c4cbf598be591924a2649f27
Mohammed Tangestani <tangestani@gmail.com>
2020-05-20 22:26:50 +0300

    Initial Commit

---
+++ MemeMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemeMe
+//
+//  Created by Mohammed Tangestani on 5/20/20.
+//  Copyright © 2020 Mohammed Tangestani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
